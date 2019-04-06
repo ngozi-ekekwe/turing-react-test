82960cfb07a939887e290716d1ed632b9e7f958f
ngozi-ekekwe <ngozi.ekekwe@andela.com>
2019-04-06 10:43:01 +0100

    remove login and register

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,3 @@ const routes = module.exports = require('next-routes')();
 routes
   .add({ name: 'home', pattern: '/', page: 'home' })
   .add({ name: 'product', pattern: '/product/:slug', page: 'product'})
-  .add({ name: 'login', pattern: '/login', page: 'login'})
-  .add({ name: 'register', pattern: '/register', page: 'register'})
