42fca7823f1a00ae86916606a51d89bf7aec4c3b
ngozi-ekekwe <ngozi.ekekwe@andela.com>
2019-04-25 22:15:22 +0100

    add discount and hove effect

--- src/components/product/ProductCard.js
+++ src/components/product/ProductCard.js
@@ -7,12 +7,15 @@ const ProductCard = ({ product }) => {
   return (
     <Link to={`/product/${product.product_id}`}>
       <a href={`/product/${product.product_id}`} className="col-lg-3 product-card">
-        <div className="thumbnail" style={{backgroundImage: `url(${imageDirectory}${product.thumbnail})`}}>
+        <div className="thumbnail" style={{ backgroundImage: `url(${imageDirectory}${product.thumbnail})` }}>
         </div>
         <p className="product-name">{product.name}</p>
-        <p className="product-price">${product.price}</p>
-        <div className="txt-align mt-4">
-          <Button text="Buy now" />
+        <div className="d-flex align-items-center justify-content-center">
+          <p className={`product-price ${product.discounted_price !== "0.00" ? 'strike' : ''}`}>${product.price}</p>
+          {product.discounted_price !== "0.00" && <p className="product-price discount pl-3">${product.discounted_price}</p>}
+        </div>
+        <div className="txt-align mt-2 buy-now-button">
+          <button className="btn">Buy now</button>
         </div>
       </a>
     </Link>
